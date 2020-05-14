0e2a4e46ac9d7a08db32553863764f8e2d8f00f1
Saulo Freire <gbsaulo@gmail.com>
2020-05-14 12:41:32 -0300

    CoverType Model

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -4,6 +4,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
         ISP_Call SP_Call { get; }
 
         void Save();
---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        public CoverType()
+        {
+        }
+        [Key]
+        public int Id { get; set; }
+        [Display(Name="Cover Type")]
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
