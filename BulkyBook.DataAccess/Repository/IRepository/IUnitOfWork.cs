4a6bcc2ed49daa3d6dd4d83a37604ce4c96421fb
Saulo Freire <gbsaulo@gmail.com>
2020-05-22 17:33:55 -0300

    Product Model/Controller

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -5,6 +5,8 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         ICoverTypeRepository CoverType { get; }
+        IProductRepository Product { get; }
+
         ISP_Call SP_Call { get; }
 
         void Save();
